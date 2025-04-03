0c198f22c035f32ea291a6364401e73ddfc19a05
venkatesh <venkateshv3548@gmail.com>
2025-04-03 02:15:56 -0500

    init commit

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,24 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-/// new changes  did
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+        // even or odd check
+
+
+        int i=2;
+        int j=7;
+
+        if(i%2==0){
+            System.out.println("number is even");
+        }else
+        {
+            System.out.println("number is odd");
+        }
+        if(j%2==0){
+            System.out.println("number is even");
+        }else
+        {
+            System.out.println("number is odd");
         }
     }
 }
